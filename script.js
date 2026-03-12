f1208f60ddfdff49ecc3a27ecccd316aee9a9de1
anawin111909 <108145027+anawin111909@users.noreply.github.com>
2026-03-12 14:24:56 +0000

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,10 @@ el.classList.add("active");
 }
 
 window.addEventListener("scroll", revealOnScroll);
+
+const toggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav-links");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
